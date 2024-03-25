9e4afb08b3fa2f4fef7decbff579e61c5b3a8388
MorganNjoroge <zeejaymorgan@morgan.com>
2024-03-25 23:52:04 +0300

    commplete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,55 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        # if items is None:
+        #     items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        self.items.append(item) if len(self.items) < self.limit else Exception('Stack is full.')
 
     def pop(self):
-        pass
+        self.items.pop() if len(self.items) > 0 else Exception('Stack is empty.')
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        print (len(self.items) >= self.limit) 
+        return len(self.items) >= self.limit 
 
     def search(self, target):
-        pass
+        try:
+            index = self.items.index(target)
+            return len(self.items) - index - 1
+        except ValueError:
+            return -1
+
+    def empty(self):
+        print (len(self.items) == 0)
+        return len(self.items) == 0  
+    
+    def __str__(self): # to convert the resulting object to a string
+        return str(self.items)
+    
+stk = Stack([1,2,3,4,5], 10)
+print(stk)
+stk.full()
+stk.empty()
+print(stk.pop())
+print('\n')
+print(stk)
+print(stk.push(7))
+print(stk)
+print('\n')
+print(stk.search(1))
+print(stk.size())
+print(stk.peek())
+print(stk.isEmpty())
